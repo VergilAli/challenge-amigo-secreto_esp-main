6a7e19a089342a61223e9afd2dddd1d498eb7f1a
Luis <alvarezluisfelipe43@gmail.com>
2025-08-11 22:42:25 -0600

    agrege funcion capturaAmigo

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 
 let amigos = [];
+
+function capturaAmigo(){
+    let nombre = document.getElementById("nombre").value.trim();
+
+    if (nombre === ""){
+        alert("Introduce un nombre valido.");
+        return;
+    }
+
+    amigos.push(nombre);
+    //Limpia la caja
+    document.getElementById("nombre").value = "";
+}
